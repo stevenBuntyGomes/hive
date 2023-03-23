9d7297ad5cb8e0bae82fdeb85bf4e0cb18fb3291
stevenBuntyGomes <stevenBuntyGomes@users.noreply.github.com>
2023-03-23 19:33:57 +0600

    prizeDetails

--- pages/details/price_details.js
+++ pages/details/price_details.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import PriceDetail from '@/components/Service/PriceDetails'
+import Clients from '../../components/Service/Clients'
+import Subscribe from '../../components/About/Subscribe'
 
 const PriceDetails = () => {
   return (
     <>
         <Layout>
-            
+            <PriceDetail/>
+            <Clients/>
+            <Subscribe/>
         </Layout>
     </>
   )
