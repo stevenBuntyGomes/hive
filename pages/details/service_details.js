730b616f8af869270076cd2dcfef2ca54e4b4f5b
stevenBuntyGomes <stevenBuntyGomes@users.noreply.github.com>
2023-03-23 18:37:14 +0600

    serviceDetails

---
+++ pages/details/service_details.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+import ServiceDetail from '../../components/Service/ServiceDetails'
+
+const ServiceDetails = () => {
+  return (
+    <>
+        <Layout>
+            <ServiceDetail/>
+        </Layout>
+    </>
+  )
+}
+
+export default ServiceDetails
