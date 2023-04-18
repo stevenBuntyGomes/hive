3a0fd28969c6009f17fa49522e1050971e952286
Zij Zahidul Islam Jiku <safzahidul.cse@gmail.com>
2023-04-18 13:55:44 +0600

    Mobile Menu button short - 01

--- components/Header/Header2.js
+++ components/Header/Header2.js
@@ -185,10 +185,23 @@ const Header2 = () => {
                         Android App
                     </Link> */}
 
-                    <div className="space-y-4 sm:flex sm:space-y-0 sm:space-x-4 mb-10">
+                    <div className="md:flex md:space-y-0 sm:space-x-4 mb-10">
+                        <a href={`${app && app.mac_app_link}`} target='_blank'
+                            className="bg-gray-800 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-2 py-[5px] mr-2 dark:bg-gray-700 dark:hover:bg-orange-500 dark:focus:ring-gray-700">
+                            <svg className="mr-1 w-2 h-2" aria-hidden="true" focusable="false" data-prefix="fab"
+                                data-icon="apple" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                                <path fill="currentColor"
+                                    d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z">
+                                </path>
+                            </svg>
+                            <div className="text-left">
+                                <div className="font-sans text-[8px] font-semibold">App Store</div>
+                            </div>
+                        </a>
+
                         <a href={`${app && app.apk_app_link}`} target='_blank'
-                            className="bg-gray-800 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-orange-500 dark:focus:ring-gray-700">
-                            <svg className="mr-3 w-7 h-7" aria-hidden="true" focusable="false" data-prefix="fab"
+                            className="bg-gray-800 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-2 py-[5px] dark:bg-gray-700 dark:hover:bg-orange-500 dark:focus:ring-gray-700">
+                            <svg className="mr-1 w-2 h-2" aria-hidden="true" focusable="false" data-prefix="fab"
                                 data-icon="google-play" role="img" xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 512 512">
                                 <path fill="currentColor"
@@ -196,23 +209,10 @@ const Header2 = () => {
                                 </path>
                             </svg>
                             <div className="text-left">
-                                <div className="mb-1 text-xs">Get in on</div>
-                                <div className="-mt-1 font-sans text-sm font-semibold">Google Play</div>
+                                <div className="font-sans text-[8px] font-semibold">Play Store</div>
                             </div>
                         </a>
-                        <a href={`${app && app.mac_app_link}`} target='_blank'
-                            className="bg-gray-800 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-orange-500 dark:focus:ring-gray-700">
-                            <svg className="mr-3 w-7 h-7" aria-hidden="true" focusable="false" data-prefix="fab"
-                                data-icon="apple" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-                                <path fill="currentColor"
-                                    d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z">
-                                </path>
-                            </svg>
-                            <div className="text-left">
-                                <div className="mb-1 text-xs">Download on the</div>
-                                <div className="-mt-1 font-sans text-sm font-semibold">Mac App Store</div>
-                            </div>
-                        </a>
+
 
                     </div>
 
